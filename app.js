a2fa180c27575da3ea7f68595a9edcf831dc21e9
Joel Strong <jdstrongpdx@gmail.com>
2024-02-09 16:38:56 -0800

    Finished adding entity pages and fixed css/images

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var db = require('./database/db-connector')
 */
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 39743;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 39744;                 // Set a port number at the top so it's easy to change in the future
 
 const { engine } = require('express-handlebars');
 var exphbs = require('express-handlebars');     // Import express-handlebars
@@ -15,6 +15,7 @@ app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the han
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
 
 /*
     ROUTES
@@ -40,6 +41,38 @@ app.get('/people', function(req, res)
         let query1 = "SELECT * FROM Households;";
         db.pool.query(query1, function(error, rows, fields){
         res.render('households', {data: rows});
+        })
+    });
+
+    app.get('/neighborhoods', function(req, res)
+    {  
+        let query1 = "SELECT * FROM Neighborhoods;";
+        db.pool.query(query1, function(error, rows, fields){
+        res.render('neighborhoods', {data: rows});
+        })
+    });
+
+    app.get('/offers', function(req, res)
+    {  
+        let query1 = "SELECT * FROM Offers;";
+        db.pool.query(query1, function(error, rows, fields){
+        res.render('offers', {data: rows});
+        })
+    });
+
+    app.get('/offerTypes', function(req, res)
+    {  
+        let query1 = "SELECT * FROM OfferTypes;";
+        db.pool.query(query1, function(error, rows, fields){
+        res.render('offerTypes', {data: rows});
+        })
+    });
+
+    app.get('/transactions', function(req, res)
+    {  
+        let query1 = "SELECT * FROM Transactions;";
+        db.pool.query(query1, function(error, rows, fields){
+        res.render('transactions', {data: rows});
         })
     });
 
