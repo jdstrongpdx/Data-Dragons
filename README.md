94e9a371c876d85b5d5e9856f239136922437f1f
Joel Strong <jdstrongpdx@gmail.com>
2024-02-09 21:56:00 -0800

    Updated views with borders, searching, credits, and css files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Data-Dragons
+
+
+Starter code provided by Dr. Curry and Prof. Safonte at Oregon State University
+https://github.com/osu-cs340-ecampus/nodejs-starter-app
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-// App.js
+// Starter code provided by Dr. Curry and Prof. Safonte at Oregon State University
+// https://github.com/osu-cs340-ecampus/nodejs-starter-app
 
 // Database
 var db = require('./database/db-connector')
