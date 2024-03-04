13e8e4792a2b317d9d9f9cc9be40b422b716ce8b
Joel Strong <jdstrongpdx@gmail.com>
2024-03-04 08:36:26 -0800

    Added delete functionality for transactions, merged Mike branch

--- public/js/add_transaction.js
+++ public/js/add_transaction.js
@@ -30,7 +30,7 @@ addTransactionForm.addEventListener("submit", function (e) {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
 
             // Add the new data to the table
-            addRowToTable(xhttp.response);
+            location.reload()
 
             // Clear the input fields for another transaction
             addTransactionForm.reset();
@@ -48,47 +48,31 @@ addTransactionForm.addEventListener("submit", function (e) {
 
 })
 
-// Creates a single row from an Object representing a single record from Transactions
-addRowToTable = (data) => {
+ function deleteTransaction(transactionId) {
+    // Put our data we want to send in a javascript object
+    let data = {
+        id: transactionId
+    };
 
-    // Get a reference to the current table on the page and clear it out.
-    let currentTable = document.getElementById("transactions-table");
-    let tbody = currentTable.getElementsByTagName("tbody")[0];
- 
-    // Get the new data
-    let parsedData = JSON.parse(data);
-    let newRow = parsedData[parsedData.length - 1]
- 
-    // Replace the content of the new row with the data we obtained
-    let row = document.createElement("TR");	   
-    let idCell = document.createElement("TD");
-    let offerIdCell = document.createElement("TD");
-    let receieverIdCell = document.createElement("TD");
-    let timeStamp = document.createElement("TD");
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("DELETE", "/delete-transaction-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
 
- 
-    // Fill the cells with correct data	
-    idCell.innerText = newRow.transactionId;	
-    offerIdCell.innerText = newRow.item;	
-    receieverIdCell.innerText = newRow.receiver;	
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 204) {
 
-    // Format the timestamp correctly
-    const dateObject = new Date(newRow.transactionTime);
-    const formattedDate = dateObject.toLocaleString();
-    timeStamp.innerText = formattedDate
+            // Add the new data to the table
+            location.reload()
 
-    // Add the cells to the row 	
-    row.appendChild(idCell);	
-    row.appendChild(offerIdCell);	
-    row.appendChild(receieverIdCell);	
-    row.appendChild(timeStamp);	
- 
-    // Unhighlight all rows 
-    for (var i = 0; i < currentTable.rows.length; i++) {
-        currentTable.rows[i].classList.remove("highlight");
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 204) {
+            console.log("There was an error with the input.")
+            var errorMsg = JSON.parse(xhttp.response);
+            alert(errorMsg.sqlMessage)
+        }
     }
-    
-    // Highlight the target row
-    row.classList.add('highlight');
-    tbody.appendChild(row);   
- }
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+}
